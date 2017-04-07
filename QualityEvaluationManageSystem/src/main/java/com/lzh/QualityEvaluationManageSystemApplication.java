88c7a3c223ed94eb35dde2e75faf13222001384d
unknown <liuzhihao@yjplan.net>
2017-04-07 09:31:54 +0800

    毕设项目框架初始化（一）

---
+++ QualityEvaluationManageSystem/src/main/java/com/lzh/QualityEvaluationManageSystemApplication.java
@@ -0,0 +1,12 @@
+package com.lzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityEvaluationManageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityEvaluationManageSystemApplication.class, args);
+	}
+}
---
+++ StudentsQualityEvaluationSystem/src/main/java/com/lzh/StudentsQualityEvaluationSystemApplication.java
@@ -0,0 +1,12 @@
+package com.lzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsQualityEvaluationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsQualityEvaluationSystemApplication.class, args);
+	}
+}
